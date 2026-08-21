0f83be2a10db7985453a28351aa7034230cafc1c
AcmeX <AcmeX@foxmail.com>
2026-08-21 14:07:41 +0800

    chore(docs): remove legacy vision_utils directory

--- docs/legacy/vision_utils/RCIA_Vision/radians_to_degrees.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def radians_to_degrees(radians):
-    return math.degrees(radians)
-
-# 使用示例
-radians_value = 3.04191
-degrees_value = radians_to_degrees(radians_value)
-print(f"{radians_value} 弧度等于 {degrees_value} 度")
